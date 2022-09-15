336a10bc13b36237bab00f1b067451c7693a9537
jimbanach <jim.banach@microsoft.com>
2022-09-15 17:14:37 -0400

    Bug Fixes and Error Handling

--- ComplianceEnvironmentPrep.ps1
+++ ComplianceEnvironmentPrep.ps1
@@ -7,6 +7,12 @@
 #########################################################
 
 #prepare the envrionment
+param ([switch]$debug,$reportpath=$env:LOCALAPPDATA)
+
+If ($debug){
+    $logpath = Join-path ($env:LOCALAPPDATA) ("CEPLOG_" + [string](Get-Date -UFormat %Y%m%d) + ".log")
+    Start-Transcript -Path $logpath
+}
 if (get-installedmodule -Name ExchangeOnlineManagement -ErrorAction SilentlyContinue) {
     Write-Host "Exchange Online Management Installed, Continuing with Script Execution"
 }
@@ -55,7 +61,8 @@ Connect-IPPSSession
 $orgname = get-user "nestor wilke" | Select-Object -expandproperty userprincipalname
 $pos = $orgname.indexof("@")
 $orgname = $orgname.substring($pos+1)
-$orgname = $orgname.TrimEnd(".onmicrosoft.com")
+$orgname = $orgname.split(".")
+$orgname = $orgname[0]
 $sposite = "https://$orgname.sharepoint.com/sites/Mark8ProjectTeam"
 
 Write-host "Connecting to SharePoint Online, Please logon in the new window" -ForegroundColor yellow
@@ -76,16 +83,64 @@ $adminusers = $tenantusers | Where-Object {$_ -notin $OOTBusers}
 
 foreach ($adminuser in $adminusers){
     $adminemail = (get-user $adminuser).userprincipalname
-    Add-RoleGroupMember -Identity "ComplianceAdministrator" -Member $adminemail
-    Add-RoleGroupMember -Identity "ediscoveryManager" -Member $adminemail
-    Add-RoleGroupMember -Identity "ComplianceManagerAdministrators" -Member $adminemail
-    Add-RoleGroupMember -Identity "InsiderRiskmanagement" -Member $adminemail
-    Add-eDiscoveryCaseAdmin -User $adminemail    
+    if(Get-RoleGroupmember -Identity "ComplianceAdministrator" | Where-Object {$_.name -in $adminuser}){
+        Write-Host "$adminuser is already a member of Compliance Administrators" -ForegroundColor Yellow
+    }
+    else{
+        Write-Host "Adding $adminuser to Compliance Administrators" -ForegroundColor Green
+        Add-RoleGroupMember -Identity "ComplianceAdministrator" -Member $adminemail
+    }
+
+    if(Get-RoleGroupmember -Identity "ediscoveryManager" | Where-Object {$_.name -in $adminuser}){
+        Write-Host "$adminuser is already a member of eDiscovery Managers" -ForegroundColor Yellow
+    }
+    else{
+        Write-Host "Adding $adminuser to eDiscovery Managers" -ForegroundColor Green
+        Add-RoleGroupMember -Identity "ediscoveryManager" -Member $adminemail
+    }
+
+    if(Get-RoleGroupmember -Identity "ComplianceManagerAdministrators" | Where-Object {$_.name -in $adminuser}){
+        Write-Host "$adminuser is already a member of Compliance Manager Administrators" -ForegroundColor Yellow
+    }
+    else{
+        Write-Host "Adding $adminuser to Compliance Manager Administrators" -ForegroundColor Green
+        Add-RoleGroupMember -Identity "ComplianceManagerAdministrators" -Member $adminemail
+    }
+    
+    if(Get-RoleGroupmember -Identity "InsiderRiskmanagement" | Where-Object {$_.name -in $adminuser}){
+        Write-Host "$adminuser is already a member of Insider Risk Management Administrators" -ForegroundColor Yellow
+    }
+    else{
+        Write-Host "Adding $adminuser to Insider Risk Management Administrators" -ForegroundColor Green
+        Add-RoleGroupMember -Identity "InsiderRiskmanagement" -Member $adminemail
+    }
+
+    if(get-ediscoverycaseadmin | Where-Object {$_.name -in $adminuser}){
+        Write-Host "$adminuser is already a member of eDiscovery Case Admins" -ForegroundColor Yellow
+    }
+    else{
+        Write-Host "Adding $adminuser to eDiscovery Case Admins" -ForegroundColor Green
+        Add-eDiscoveryCaseAdmin -User $adminemail
+    }
 }
 
 #upload files to sharepoint
 $temppath = $env:LOCALAPPDATA+"\mark8"+[string](Get-Date -UFormat %Y%m%d%S)
 $tempfile = $temppath +".zip"
+
+#make sure the SPO Site is actually accessible
+Write-Host "Checking Connectivity to SharePoint Online" -ForegroundColor Yellow
+
+try {get-pnpsite -ErrorAction stop > $null
+}
+catch {
+    Write-Host "Unable to connect to site $sposite.  Please check the URL and try again later" -ForegroundColor Red
+    Write-Host "Disconnecting Sessions for Cleanup" -ForegroundColor Green
+    Disconnect-ExchangeOnline -Confirm:$false -ErrorAction:SilentlyContinue -InformationAction Ignore
+    Disconnect-PnPOnline -InformationAction Ignore
+    Exit
+}
+Write-Host "Successfuly connected to $sposite" -ForegroundColor Green
 
 (New-Object Net.WebClient).DownloadFile("https://github.com/microsoft/CompliancePartnerWorkshops/raw/main/mark8.zip", "$tempfile")
 Expand-Archive -LiteralPath $tempfile -DestinationPath $temppath -Force
@@ -110,14 +165,30 @@ Remove-Item $tempfile
 #create our DLP Policies
 
 #create SITs
+#we are looking for the existence of this specific rule package as a catch before creating it
+if((Get-DlpSensitiveInformationTypeRulePackage).RuleCollectionName -match "MRUS Rule Package"){
+    Write-Host "Sensitive Information Types already exist, Continuing with Script" -ForegroundColor Yellow
+}
+
+else{
 Write-Host "Creating Sensitive Information Types" -ForegroundColor Yellow
 $rulefile = $env:LOCALAPPDATA+"\rulepack"+[string](Get-Date -UFormat %Y%m%d%S)+".xml"
 (New-Object Net.WebClient).DownloadFile("https://github.com/microsoft/CompliancePartnerWorkshops/raw/main/rulepack.xml", "$rulefile")
-New-DlpSensitiveInformationTypeRulePackage -FileData ([System.IO.File]::ReadAllBytes($rulefile))
+New-DlpSensitiveInformationTypeRulePackage -FileData ([System.IO.File]::ReadAllBytes($rulefile)) > $null
 
 Remove-item $rulefile
+}
+
+$sitrules = Get-DlpSensitiveInformationType | Where-Object {$_.Publisher -match "Microsoft Partner Accelerators"}
+Foreach($sit in $sitrules){
+    Write-Host "$($sit.name) verified successfully" -ForegroundColor Green
+}
 
 #create DLP Policies
+if((Get-DlpCompliancePolicy).name -match "Project Obsidian DLP Policy"){
+    Write-Host "Project Obsidian DLP Policy Already Exists" -ForegroundColor Yellow
+}
+else{
 Write-Host "Creating DLP Policies" -ForegroundColor Yellow
 New-DlpCompliancePolicy -Name "Project Obsidian DLP Policy" `
                         -Mode Enable `
@@ -125,30 +196,43 @@ New-DlpCompliancePolicy -Name "Project Obsidian DLP Policy" `
                         -SharePointLocation All `
                         -OneDriveLocation All `
                         -TeamsLocation All `
-                        -Comment "Policy created during workshop lab on $(Get-Date -Format D)"
+                        -Comment "Policy created for RiskInvestigator on $(Get-Date -Format D)" `
+                        > $null
 
 New-DlpComplianceRule   -Name "Project Obsidian DLP policy rule" `
                         -Policy "Project Obsidian DLP Policy" `
                         -ContentContainsSensitiveInformation @{Name="Project Obsidian";minCount="1"} `
                         -GenerateAlert True `
                         -AlertProperties @{AggregationType="None"} `
-                        -ReportSeverityLevel High
-
+                        -ReportSeverityLevel High `
+                        > $null
+}
+if((Get-DlpCompliancePolicy).name -match "Project Olivine DLP Policy"){
+    Write-Host "Project Olivine DLP Policy Already Exists" -ForegroundColor Yellow
+}
+else{
 New-DlpCompliancePolicy -Name "Project Olivine DLP Policy" `
                         -Mode Enable `
                         -ExchangeLocation All `
                         -SharePointLocation All `
                         -OneDriveLocation All `
                         -TeamsLocation All `
-                        -Comment "Policy created during workshop lab on $(Get-Date -Format D)"
+                        -Comment "Policy created for RiskInvestigator on $(Get-Date -Format D)" `
+                        > $null
 
 New-DlpComplianceRule   -Name "Project Olivine DLP policy rule" `
                         -Policy "Project Olivine DLP Policy" `
                         -ContentContainsSensitiveInformation @{Name="Project Olivine";minCount="1"} `
                         -GenerateAlert True `
                         -AlertProperties @{AggregationType="None"} `
-                        -ReportSeverityLevel High
-                        
+                        -ReportSeverityLevel High `
+                        > $null
+} 
+$dlpcompliancepolicies = get-DlpCompliancePolicy | Where-Object {$_.Comment -match "RiskInvestigator*"}
+Foreach($dlppolicy in $dlpcompliancepolicies){
+    Write-Host "$($dlppolicy.name) verified successfully" -ForegroundColor Green
+}
+
 ### we check for the admin audit log as the last piece of this.  
 ### this way we keep the exchangeonline and security and compliance
 ### cmdlets from stomping on each other
@@ -161,18 +245,42 @@ if ((Get-AdminAuditLogConfig).UnifiedAuditLogIngestionEnabled -match "False"){
         Write-Host "Organization Customization is not enabled. Enabling, please wait" -ForegroundColor Magenta
         Enable-OrganizationCustomization
     }
-    Write-Host "Enabling the Unified Audit Log" -ForegroundColor Green  
-    Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
+    
+    If ((Get-OrganizationConfig).IsDehydrated -match "True"){
+        $i=1
+        Do{
+            Write-Host "Confirming Organization Configuration is Complete, Check ($i of 5)" -ForegroundColor Yellow
+            $orgstatus = (Get-OrganizationConfig).isDehydrated
+            start-sleep 30
+            $i++
+        } While ($orgstatus -contains 'True' -and $i -lt 6) 
     }
 
-    Else{
+    If ((Get-OrganizationConfig).IsDehydrated -match "False"){
+        Write-Host "Enabling the Unified Audit Log" -ForegroundColor Green  
+        Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
+    }
+    Else {
+        Write-Host "Unable to automatically enable unified audit log. `nPlease enable manually in Microsoft Purview Compliance Portal" -ForegroundColor Red
+        Write-Host "Disconnecting Sessions for Cleanup" -ForegroundColor Green
+        Disconnect-ExchangeOnline -Confirm:$false -ErrorAction:SilentlyContinue -InformationAction Ignore
+        Disconnect-PnPOnline -InformationAction Ignore
+        Exit
+    }
+    }
+
+Else{
     write-host "Unified Audit Log is Enabled, Continuing with Script" -ForegroundColor Yellow
-}
+    }
 
 Write-Host "Disconnecting Sessions for Cleanup" -ForegroundColor Green
 Disconnect-ExchangeOnline -Confirm:$false -ErrorAction:SilentlyContinue -InformationAction Ignore
 Disconnect-PnPOnline -InformationAction Ignore
 
-### need to update these with variables when fixing up the other code ##
-#start-process "https://compliance.microsoft.com/"
-#start-process "https://6glf4w.sharepoint.com/sites/Mark8ProjectTeam/Shared%20Documents/"
+start-process "https://compliance.microsoft.com/"
+start-process "https://$orgname.sharepoint.com/sites/Mark8ProjectTeam/Shared%20Documents/"
+
+If ($debug){
+    $logpath = Join-path ($env:LOCALAPPDATA) ("CEPLOG_" + [string](Get-Date -UFormat %Y%m%d) + ".log")
+    Stop-Transcript -ErrorAction SilentlyContinue
+}
